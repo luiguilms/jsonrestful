9adf34158e8b48770a7bcd230bff7e1be9c86ed5
Luigui Marco Lupacca Apaza <luigui.lupacca@tecsup.edu.pe>
2023-11-28 16:50:05 -0500

    Initial Commit

---
+++ JSONRESTful/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JSONRESTful
+//
+//  Created by Luigui Lupacca on 28/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
